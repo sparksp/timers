ad56fdffd53d93a8cc9b3e52eb107431dc192145
Phill Sparks <me@phills.me.uk>
2020-04-16 15:53:53 +0100

    Use yarn instead of npm

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,17 @@
 module.exports = {
     plugins: [
-        require("tailwindcss")('tailwind.config.js'),
-        require("postcss-elm-tailwind")({
-            elmFile: "src/Html/Tailwind.elm",
-            elmModuleName: "Html.Tailwind",
-        }),
-        require("postcss-elm-tailwind")({
-            elmFile : "src/Svg/Tailwind.elm",
-            elmModuleName: "Svg.Tailwind",
-            svg: true
-        }),
-        require("autoprefixer")
+        require('tailwindcss')('tailwind.config.js'),
+        require('postcss-elm-tailwind')(
+            { elmFile: 'src/Html/Tailwind.elm'
+            , elmModuleName: 'Html.Tailwind'
+            , formats:
+                { svg:
+                    { elmFile: 'src/Svg/Tailwind.elm'
+                    , elmModuleName: 'Svg.Tailwind'
+                    }
+                }
+            }
+        ),
+        require('autoprefixer')
     ]
 };
